8d26bb014a51c5929b81aa9a0c596f9e4d99fd63
JohnPetros <joaopedro.nc@outlook.com>
2024-12-18 12:29:55 -0300

    🪨 constants: add HTTP_STATUS_CODE

---
+++ src/@core/constants/http-status-code.ts
@@ -0,0 +1,11 @@
+export const HTTP_STATUS_CODE = {
+  ok: 200,
+  created: 201,
+  noContent: 204,
+  redirect: 301,
+  badRequest: 400,
+  unauthorized: 401,
+  notFound: 404,
+  conflict: 409,
+  serverError: 500,
+}
