3f6e75c2cd87a8f04a677dea8a1c3658b86b44b7
JohnPetros <joaopedro.nc@outlook.com>
2024-12-18 17:30:39 -0300

    🪨 constants: add cache keys

---
+++ src/@core/constants/cache-keys.ts
@@ -0,0 +1,4 @@
+export const CACHE_KEYS = {
+  accessToken: 'access_token',
+  refreshToken: 'refresh_token',
+}
--- src/@core/constants/index.ts
+++ src/@core/constants/index.ts
@@ -1,1 +1,2 @@
 export { HTTP_STATUS_CODE } from './http-status-code'
+export { CACHE_KEYS } from './cache-keys'
