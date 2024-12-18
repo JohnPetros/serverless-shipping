2ab27450e97298d0a8ec791977b9e0ba6bcdbb16
JohnPetros <joaopedro.nc@outlook.com>
2024-12-18 17:32:05 -0300

    ➕ providers: implement cache provider using Redis

--- src/@core/interfaces/index.ts
+++ src/@core/interfaces/index.ts
@@ -1,3 +1,4 @@
 export type { Http, HttpSchema } from './http'
 export type { ShippingProvider } from './shipping-provider'
 export type { ApiClientProvider } from './api-client-provider'
+export type { CacheProvider } from './cache-provider'
---
+++ src/providers/cache-provider/redis/redis-cache-provider.ts
@@ -0,0 +1,28 @@
+import Redis from 'ioredis'
+
+import type { CacheProvider } from '@core/interfaces'
+
+export class RedisCacheProvider implements CacheProvider {
+  private redis: Redis
+
+  constructor() {
+    this.redis = new Redis(
+      'rediss://default:AYGwAAIjcDFiMjMyZGUyODE0ZDc0NTJlYTM1YWIzYzg4ZDQxNjFlZXAxMA@many-wombat-33200.upstash.io:6379',
+    )
+  }
+
+  async set(key: string, data: unknown): Promise<void> {
+    await this.redis.set(key, JSON.stringify(data))
+  }
+
+  async get<Data>(key: string): Promise<Data | null> {
+    const data = await this.redis.get(key)
+    if (data) return JSON.parse(data)
+
+    return null
+  }
+
+  async delete(key: string): Promise<void> {
+    await this.redis.del(key)
+  }
+}
