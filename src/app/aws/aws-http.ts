aea288a282a059989c6f1df1545e8c0ad5e71156
JohnPetros <joaopedro.nc@outlook.com>
2024-12-18 16:05:17 -0300

    🪨 config: handle global erros in middify

--- src/app/aws/aws-http.ts
+++ src/app/aws/aws-http.ts
@@ -1,6 +1,7 @@
 import type { Http } from 'src/@core/interfaces/index'
 import type { AwsEvent, AwsSchema } from '@app/aws/types'
 import { HTTP_STATUS_CODE } from '@core/constants/http-status-code'
+import { AppError } from '@core/errors'
 
 export class AwsHttp<Schema extends AwsSchema>
   implements
@@ -20,17 +21,22 @@ export class AwsHttp<Schema extends AwsSchema>
   }
 
   getBody() {
-    if (!this.event.body) throw Error()
+    if (!this.event.body) throw new AppError('Validation Error', 'Query Params not found')
+
     return this.event.body
   }
 
   getRouteParams() {
-    if (!this.event.body) throw Error()
+    if (!this.event.pathParameters)
+      throw new AppError('Validation Error', 'Query Params not found')
+
     return this.event.pathParameters
   }
 
   getQueryParams() {
-    if (!this.event.body) throw Error()
+    if (!this.event.queryStringParameters)
+      throw new AppError('Validation Error', 'Query Params not found')
+
     return this.event.queryStringParameters
   }
 }
--- src/app/middyfy.ts
+++ src/app/middyfy.ts
@@ -2,7 +2,17 @@ import middy from '@middy/core'
 import validator from '@middy/validator'
 import { transpileSchema } from '@middy/validator/transpile'
 import middyJsonBodyParser from '@middy/http-json-body-parser'
+
+import { ApiError, AppError } from '@core/errors'
+import { HTTP_STATUS_CODE } from '@core/constants'
 import type { AwsEvent } from './aws/types'
+
+type ErrorResponse = {
+  title: string
+  message: string
+  statusCode: number
+  validationError?: unknown
+}
 
 export function middyfy(awsFunction: (event: AwsEvent) => void, schema?: object) {
   const middlewares = [middyJsonBodyParser()]
@@ -14,5 +24,38 @@ export function middyfy(awsFunction: (event: AwsEvent) => void, schema?: object)
       }),
     )
 
-  return middy(awsFunction).use(middlewares)
+  return middy(awsFunction)
+    .use(middlewares)
+    .onError(({ error }) => {
+      if (!error) return
+
+      const errorResponse: ErrorResponse = {
+        title: error.name,
+        message: error.message,
+        statusCode: HTTP_STATUS_CODE.serverError,
+      }
+
+      if (error && 'cause' in error) {
+        errorResponse.title = 'Validation Error'
+        errorResponse.message = error.message
+        errorResponse.statusCode = HTTP_STATUS_CODE.badRequest
+        errorResponse.validationError = error.cause
+      }
+
+      if (error instanceof AppError) {
+        errorResponse.title = error.title
+        errorResponse.message = error.message
+
+        if (error instanceof ApiError) {
+          errorResponse.title = error.title
+          errorResponse.message = error.message
+          errorResponse.statusCode = error.statusCode
+        }
+      }
+
+      return {
+        statusCode: errorResponse.statusCode,
+        body: JSON.stringify(errorResponse),
+      }
+    })
 }
