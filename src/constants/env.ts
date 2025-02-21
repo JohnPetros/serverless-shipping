899093083b968041b444722cc9bfa61d5d0a1a43
JohnPetros <joaopedro.nc@outlook.com>
2025-02-21 14:32:29 -0300

    ⚙️ config: add REDIS_TOKEN env var

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,7 +3,8 @@ if (
   !process.env.MELHOR_ENVIO_CLIENT_ID ||
   !process.env.MELHOR_ENVIO_SECRET ||
   !process.env.MELHOR_ENVIO_URL ||
-  !process.env.REDIS_URL
+  !process.env.REDIS_URL ||
+  !process.env.REDIS_TOKEN
 )
   throw Error('Enviroment variables are not set')
 
@@ -13,6 +14,7 @@ const ENV = {
   melhorEnvioSecret: process.env.MELHOR_ENVIO_SECRET,
   melhorEnvioUrl: process.env.MELHOR_ENVIO_URL,
   redisUrl: process.env.REDIS_URL,
+  redisToken: process.env.REDIS_TOKEN,
 }
 
 export { ENV }
